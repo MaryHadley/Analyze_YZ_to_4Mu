14795f5f2bf30152d35bb6eed62cfe37917cae68
Mary Hadley <mary.hadley@cern.ch>
2023-07-08 09:01:08 +0200

    almost complete Punzi significance calculations included

--- PunziSignificance/computePunziSignificance.py
+++ PunziSignificance/computePunziSignificance.py
@@ -31,3 +31,39 @@ def compute_S_min_imp(a,b,B):
 # print compute_S_min (a,b,B)
 
 
+#print compute_S_min(a=1, B=1, b=2) #returns the same thing as compute_S_min(a=1, b=2, B=2), as it should
+
+#Stat Comm suggests a=2, b=5, so we will use those
+
+#Upsi mu pT cut of 2 case
+#a =2 
+#b = 5
+#B = 159 + 68. 4 = 237.4
+
+print "Upsi mu pT cut of 2 case:"
+print "########################"
+print "S_min:  ", compute_S_min(a=2, b=5, B=237.4)
+print "S_min_imp:  ", compute_S_min_imp(a=2, b=5, B=237.4)
+print "########################"
+
+#Upsi mu pT cut of 3 case
+#a =2
+#b = 5
+#B = 88.9 + 48.1 = 137
+
+print "Upsi mu pT cut of 3 case:"
+print "########################"
+print "S_min:  ", compute_S_min(a=2, b=5, B=137)
+print "S_min_imp:  ", compute_S_min_imp(a=2, b=5, B=137)
+print "#######################"
+
+#Upsi mu pT cut of 4 case
+#a = 2
+#b = 5
+#B = 42.4 + 25.3 = 67.7
+
+print "Upsi mu pT cut of 4 case:"
+print "########################"
+print "S_min:  ", compute_S_min(a=2, b=5, B = 67.7)
+print "S_min_imp:  ", compute_S_min_imp(a=2, b=5, B = 67.7)
+print "########################"
